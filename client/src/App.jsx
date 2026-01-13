9db2893561cd57d913a472ef1ab0d6ab05272a7f
Irving Bautista <irvingbautista681@gmail.com>
2026-01-13 12:24:37 -0800

    add Tailwind CSS for styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react"
+
 function App() {
+  const [count, setCount] = useState(0)
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="flex flex-col items-center gap-6">
+      <h1 className="text-3xl font-bold underline">Hello</h1>
+      <h2 className="">Count {count}</h2>
+      <button className="rounded-xl p-2 bg-sky-500  text-white hover:bg-sky-700 shadow-lg outline outline-black/5 cursor-pointer" 
+        onClick={() => setCount(prev => prev + 1)}>
+          Click me
+      </button>
+    </div>
   )
 }
 
